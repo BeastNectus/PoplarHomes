1bd8477d3c2ce52a400f67be36543c3e3cdf2849
John Mamanao <kingfrost1143@gmail.com>
2024-07-08 11:11:07 +0800

    small changes

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ function App() {
     <ApolloProvider client={client}>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Login" component={LoginScreen} />
+          {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
           <Stack.Screen name="Home" component={HomeScreen} />
         </Stack.Navigator>
       </NavigationContainer>
